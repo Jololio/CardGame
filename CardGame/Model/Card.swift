fbcfd41a0b0ce67acce3d90aa6debdc8ee71d50e
Jololio <jala9132@740s-pl-120.local>
2016-10-25 13:27:13 -0600

    getter methods for card class

--- CardGame/Model/Card.swift
+++ CardGame/Model/Card.swift
@@ -19,5 +19,21 @@ class Card
         isFaceUp = false
     }
     
+    func getBackImage() -> UIImage
+    {
+        return backImage
+    }
+    
+    func isFacing() -> Bool
+    {
+        return isFaceUp
+    }
+    
+    func toString() -> String
+    {
+        let description = "This card is \(isFaceUp)"
+        
+        return description
+    }
     
 }
