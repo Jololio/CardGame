b4c371399f49762676316eef4e59986f52b1c1f1
Jololio <jala9132@740s-pl-120.local>
2016-11-02 13:21:44 -0600

    Fixed Errors
    Fixed the Super. location error and fixed the getter/setter errors

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -11,56 +11,22 @@ import UIKit
 class PlayingCard : Card
 {
     internal var suit : String
-    {
-        get
-        {
-            return self.suit
-        }
-        set(suit)
-        {
-            self.suit = suit
-        }
-    }
-    
     internal var color : UIColor
-    {
-        get
-        {
-            return self.color
-        }
-        set(color)
-        {
-            self.color = color
-        }
-        
-    }
-    
     internal var rank : Int
-    {
-        get
-        {
-            return self.rank
-        }
-        set(rank)
-        {
-            self.rank = rank
-        }
-    }
-    
     override init()
     {
-        super.init()
         rank = 0
         suit = String()
         color = UIColor.redColor()
+        super.init()
     }
     
     init(withRank: Int, ofSuit:String)
     {
-        super.init()
         color = UIColor.redColor()
         suit = ofSuit
         rank = withRank
+        super.init()
     }
     
     func getColor() -> UIColor
