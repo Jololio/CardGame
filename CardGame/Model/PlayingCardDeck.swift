16242fc9bbc939295058e764d7686cedeac78aac
Joseph Al-Abudi <jala9132@740s-pl-120.local>
2016-12-16 11:47:37 -0700

    updated loops to Swift 3

--- CardGame/Model/PlayingCardDeck.swift
+++ CardGame/Model/PlayingCardDeck.swift
@@ -16,7 +16,7 @@ class PlayingCardDeck : Deck
         
         for suit in PlayingCard.validSuits()
         {
-            for var rank = 1; rank <= PlayingCard.maxRank(); rank += 1
+            for rank in 1...PlayingCard.maxRank()
             {
                 let tempCard = PlayingCard(withRank: rank, ofSuit: suit)
                 cards.append(tempCard)
@@ -30,7 +30,7 @@ class PlayingCardDeck : Deck
         var temp = [PlayingCard]()
         for suit in PlayingCard.validSuits()
         {
-            for var rank = 1; rank <= PlayingCard.maxRank(); rank += 1
+            for rank in 1...PlayingCard.maxRank()
             {
                 let index = cards.index(
                     where: {($0 as! PlayingCard).suit == suit && ($0 as! PlayingCard).rank == rank})
